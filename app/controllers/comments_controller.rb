7a91525b1d9efee17d0ee965f34221f7e8050832
Anthony Catullo <acatullo4@gmail.com>
2021-06-16 19:34:51 -0500

    add crud for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,36 @@
 class CommentsController < ApplicationController
+
+    def index 
+        comments = Comment.all
+        render json: comments
+    end 
+
+    def create 
+        comment = Comment.new(comment_params)
+
+        if comment.save 
+            render json: comment 
+        end 
+    end 
+
+    def update 
+        comment = Comment.find(params[:id])
+        comment.update(comment_params)
+        render json: comment
+    end 
+
+    def destroy
+        comment = Comment.find(params[:id])
+        comment.destroy
+        head :ok
+    end 
+
+    
+
+
+    private 
+
+    def comment_params 
+        params.require(:comment).permit(:content, :home_id)
+    end 
 end
